d515463179e0719620fea7025ec9c5144f73f03b
omoling <omoling@d3abeef6-d5fd-11de-8017-1f6ee2e63914>
2009-11-26 13:49:41 +0000

    added javascript, could not test yet

--- trunk/iwa_alpha/WebContent/script.js
+++ trunk/iwa_alpha/WebContent/script.js
@@ -21,27 +21,107 @@ function getXMLObject()  //XML OBJECT
 var xmlhttp = new getXMLObject();	//xmlhttp holds the ajax object
  
 function ajaxFunction() {
-  var getdate = new Date();  //Used to prevent caching during ajax call
+	//put containers in loading state
+	containersLoading();
+	
   if(xmlhttp) { 
 	var searchString = document.getElementById("search").value;
-	alert(searchString);
     xmlhttp.open("GET","MainServlet?search=" + searchString, true);
     xmlhttp.onreadystatechange  = handleServerResponse;
     xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xmlhttp.send(null);
+  } else {
+	  alert("AJAX error!");
+	  containersLoaded();  
   }
 }
  
 function handleServerResponse() {
    if (xmlhttp.readyState == 4) {
      if(xmlhttp.status == 200) {
-       //update content
-       document.getElementById("artist").innerHtml = xmlhttp.responseXml;
+    	 //call content dispatcher
+    	 contentHandler(xmlHttp.responseXml);
      }
      else {
-        //alert("Error during AJAX call. Please try again");
+    	 //alert("Error during AJAX call. Please try again");
      }
    }
+}
+
+function contentHandler(xml){
+	
+	//load XSL
+	//TODO: extend for all XSL
+	var xsl=loadXMLFile("rdf2html.xsl");
+	
+	//html content var
+	//TODO: extend for all containers
+	var htmlContent = "";
+	
+	//TODO: run for all containers-xsl
+	if (window.ActiveXObject)
+	   {
+	     htmlContent = xml.transformNode(xsl);
+	   }
+	   // code for Mozilla, Firefox, Opera, etc.
+	   else if (document.implementation
+	   && document.implementation.createDocument)
+	   {
+	     xsltProcessor = new XSLTProcessor();
+	     xsltProcessor.importStylesheet(xsl);
+	     htmlContent = xsltProcessor.transformToFragment(xml,document);
+	   }	
+	
+	//update containers
+	//TODO: update all containers
+	updateContainer('artist', htmlContent);
+	
+}
+
+function loadXMLFile(filename)
+{
+  var xmlDoc;
+  // code for IE
+  if (window.ActiveXObject)
+  {
+    xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+  }
+  // code for Mozilla, Firefox, Opera, etc.
+  else if (document.implementation
+  && document.implementation.createDocument)
+  {
+    xmlDoc=document.implementation.createDocument("","",null);
+   }
+  else
+  {
+    alert('Your browser cannot handle this script');
+  }
+xmlDoc.async=false;
+xmlDoc.load(filename);
+return(xmlDoc);
+}
+
+function updateContainer(id, newContent){
+	document.getElementById(id).innerHTML = newContent;
+	loaded(id);
+}
+
+function containersLoading(){
+	//for now: all
+	isLoading('artist');
+	isLoading('event');
+	isLoading('map');
+	isLoading('video');
+	isLoading('image');
+}
+
+function containersLoaded(){
+	//for now: all
+	loaded('artist');
+	loaded('event');
+	loaded('map');
+	loaded('video');
+	loaded('image');
 }
 
 function isloading(id){
@@ -51,3 +131,6 @@ function isloading(id){
 function loaded(id) {
 	document.getElementById(id+'-hover').style.display = 'none';
 }
+
+
+
