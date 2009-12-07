5b8b744f37465f580077574c728002dbdb9c85bc
psylus <psylus@d3abeef6-d5fd-11de-8017-1f6ee2e63914>
2009-12-07 12:40:49 +0000

    Final model implemented for band search, returning basic info from dbpedia

--- trunk/iwa_alpha/src/iwa1/datasources/DataDBpedia.java
+++ trunk/iwa_alpha/src/iwa1/datasources/DataDBpedia.java
@@ -22,34 +22,38 @@ public class DataDBpedia {
 	public static Model dbpedia_model = ModelFactory.createDefaultModel();
 	public static void query_dbpedia(String keyword) {
 		String queryString=
-			"PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
-            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "+
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "+
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
-            "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "+
-            "PREFIX dc: <http://purl.org/dc/elements/1.1/> "+
-            "PREFIX dbpedia2: <http://dbpedia.org/property/> "+
-            "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> "+
-            "PREFIX dbpedia: <http://dbpedia.org/resource/> "+
-            "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> "+
-			"CONSTRUCT { "+ 
-			"?s rdf:type foaf:Agent; "+
-			"foaf:name ?name; "+
-			"dbpedia2:abstract ?abstract; "+
-			"dbpedia2:genre ?genre; "+
-			"dbpedia-owl:thumbnail ?thumbnail; "+
-			"dbpedia2:url ?url.} "+
-			"WHERE { "+
-			"GRAPH ?g{" +
-			"?s a dbpedia-owl:Artist. "+
-			"?s foaf:name ?name. "+
-			"?s dbpedia2:abstract ?abstract. "+
-			"?s  dbpedia2:genre ?genre. "+
-			"?s  dbpedia-owl:thumbnail ?thumbnail. "+
-			"?s  dbpedia2:url ?url .} "+
-			"FILTER (regex(?name, \""+keyword+"\")). "+
-			"FILTER langMatches( lang(?abstract), 'en') "+
-			"}";
+		        "PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "+
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "+
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "+
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/> "+
+                "PREFIX dbpedia2: <http://dbpedia.org/property/> "+
+                "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> "+
+                "PREFIX dbpedia: <http://dbpedia.org/resource/> "+
+                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> "+
+				"CONSTRUCT { "+
+				"?s rdf:type foaf:Agent; "+
+				"foaf:name ?name; "+
+				"dbpedia2:abstract ?abstract; "+
+				"dbpedia2:currentMembers ?currentMembers; "+
+				"dbpedia2:yearsActive ?yearsActive; "+
+				"dbpedia2:genre ?genre; "+
+				"dbpedia-owl:thumbnail ?thumbnail; "+
+				"dbpedia2:url ?url.} "+
+				"WHERE {" +
+				"GRAPH ?g{ "+
+				"?s a dbpedia-owl:Band. "+
+				"?s foaf:name ?name. "+
+				"?s dbpedia2:abstract ?abstract. "+
+				"?s  dbpedia-owl:thumbnail ?thumbnail. "+
+				"?s dbpedia2:currentMembers ?currentMembers. "+
+				"?s dbpedia2:yearsActive ?yearsActive. "+
+				"?s  dbpedia2:genre ?genre. "+
+				"?s  dbpedia2:url ?url. "+
+				"FILTER (regex(?name, \""+keyword+"\")). "+
+				"FILTER langMatches( lang(?abstract), 'en'). "+
+				"}.}";
               
 		// Create a query object
 		Query query = QueryFactory.create(queryString);
@@ -99,6 +103,14 @@ public class DataDBpedia {
 		dbpedia_model.write(rdf_stream);
 		return rdf_stream.toString();
 	
+	}
+	
+	public static void addNflush()
+	{
+		//Add flickr model to applications main model and clear data
+		JenaFrame.model.add(dbpedia_model);
+		//clear data
+		dbpedia_model.removeAll();
 	}
 	
 
